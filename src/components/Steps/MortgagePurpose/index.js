7f34d4a2e9381d8a800a462148cfc6a12a5c5864
Alex Muller <alex@innocells.io>
2019-03-04 11:47:57 +0100

    Add Button styles

--- src/components/Steps/MortgagePurpose/index.js
+++ src/components/Steps/MortgagePurpose/index.js
@@ -14,6 +14,7 @@ const MortagePurpose = ({ onSelectOption, stepTitle }) => (
           }}
         >
           Vivienda habitual
+          <i className="fa fa-home" />
         </Button>
       </Col>
     </Row>
