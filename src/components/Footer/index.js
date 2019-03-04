25ebe50d34e9f4bd1294805e8be430a36aa75dca
Alex Muller <alex@innocells.io>
2019-03-04 11:34:42 +0100

    Add footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import './styles.css';
+import logo from './logo.svg';
 
-export default () => <div>Powered by Trioteca</div>;
+export default () => (
+  <div className="footer">
+    Powered by Trioteca
+    <img src={logo} alt="Trioteca" className="logo" />
+  </div>
+);
