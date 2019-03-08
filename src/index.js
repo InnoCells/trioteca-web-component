9a4c421ba3229f86b63c599215f5ab76ac5bd863
Alex Muller <alex@innocells.io>
2019-03-08 10:17:58 +0100

    Get URL query parameters

--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@ import './index.css';
 
 import App from './App';
 
+const urlParams = new URLSearchParams(window.location.search);
+const price = parseInt(urlParams.get('price'), 10);
+const provinceId = parseInt(urlParams.get('provinceId'), 10);
+const source = urlParams.get('source');
+
+if (Number.isNaN(price)) {
+  throw new Error('Invalid parameter "price"');
+}
+
+if (Number.isNaN(provinceId)) {
+  throw new Error('Invalid parameter "provinceId"');
+}
+
 ReactDOM.render(
   <I18nextProvider i18n={i18next}>
-    <App price={200000} provinceId={10} />
+    <App price={price} provinceId={provinceId} source={source} />
   </I18nextProvider>,
   document.getElementById('root')
 );
